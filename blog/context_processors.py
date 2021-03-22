878865f2f570615eb7c7c2d1577b2bb976c592a3
laguimarfh <laguimarfh@gmail.com>
2021-03-22 00:27:49 -0400

    imagem corrigida

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -4,7 +4,8 @@ from . import models
 
 def base_context(request):
     artigos = models.Artigo.objects.filter(status='published')   
-    
+    artigo_destaque = models.Artigo.objects.filter(destaque=True)   
     return {
         'artigos': artigos,
+        'artigo_destaque': artigo_destaque,
     }
